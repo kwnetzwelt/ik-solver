a7b084b525888fb249d2a70fe3d487abdfa65d11
Kai Wegner <kai.wegner@exozet.com>
2018-01-29 16:30:04 +0100

    * updated to unity version 2017.3.0f3 * renamed classes to CamelCase * moved all classes to IK Namespace

---
+++ Assets/Scripts/IK/JointRotate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IK
+{
+    public class JointRotate : MonoBehaviour {
+
+        private float time = 0f;
+        private int intime = 1;
+
+        // Use this for initialization
+        void Start () {
+            
+        }
+        
+        // Update is called once per frame
+        void Update () {
+            if (intime > time)
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, time));
+            }
+
+            time += Time.deltaTime;
+            intime = ((int)time) + 1;
+
+        }
+    }
+}
